8f16c241ce4d0303792a66f280d81401396977e6
poolsawatapin@gmail.com <poolsawatapin@gmail.com@47c1b8f4-fa85-6f62-4905-8be3210b9e9d>
2014-07-06 11:26:24 +0000

    commit

--- trunk/app/protected/config/main.php
+++ trunk/app/protected/config/main.php
@@ -10,8 +10,8 @@ return array(
     'preload' => array('log', 'booster'),
     'aliases' => array(
         'booster' => realpath(__DIR__ . '/../extensions/booster'), // change this if necessary
-        //'bootstrap' => realpath(__DIR__ . '/../extensions/bootstrap'), // change this if necessary
-       // 'yiiwheels' => realpath(__DIR__ . '/../extensions/yiiwheels'), // change if necessary
+    //'bootstrap' => realpath(__DIR__ . '/../extensions/bootstrap'), // change this if necessary
+    // 'yiiwheels' => realpath(__DIR__ . '/../extensions/yiiwheels'), // change if necessary
     ),
     // preloading 'log' component
 //    'preload' => array('log'),
@@ -22,7 +22,7 @@ return array(
         'booster.helpers.TbHtml',
 //        'bootstrap.helpers.TbHtml',
 //        'bootstrap.helpers.TbArray',
-        //'bootstrap.behaviors.TbWidget',
+    //'bootstrap.behaviors.TbWidget',
     ),
     'modules' => array(
         'gii' => array(
@@ -35,6 +35,49 @@ return array(
     ),
     // application components
     'components' => array(
+        /*
+         * report pdf
+         */
+        'ePdf' => array(
+            'class' => 'ext.yii-pdf.EYiiPdf',
+            'params' => array(
+                'mpdf' => array(
+                    'librarySourcePath' => 'application.vendor.mpdf.*',
+                    'constants' => array(
+                        '_MPDF_TEMP_PATH' => Yii::getPathOfAlias('application.runtime'),
+                    ),
+                    'class' => 'mpdf', // the literal class filename to be loaded from the vendors folder
+                /* 'defaultParams'     => array( // More info: http://mpdf1.com/manual/index.php?tid=184
+                  'mode'              => '', //  This parameter specifies the mode of the new document.
+                  'format'            => 'A4', // format A4, A5, ...
+                  'default_font_size' => 0, // Sets the default document font size in points (pt)
+                  'default_font'      => '', // Sets the default font-family for the new document.
+                  'mgl'               => 15, // margin_left. Sets the page margins for the new document.
+                  'mgr'               => 15, // margin_right
+                  'mgt'               => 16, // margin_top
+                  'mgb'               => 16, // margin_bottom
+                  'mgh'               => 9, // margin_header
+                  'mgf'               => 9, // margin_footer
+                  'orientation'       => 'P', // landscape or portrait orientation
+                  ) */
+                ),
+                'HTML2PDF' => array(
+                    'librarySourcePath' => 'application.vendor.html2pdf.*',
+                    'classFile' => 'html2pdf.class.php', // For adding to Yii::$classMap
+                    'defaultParams' => array(// More info: http://wiki.spipu.net/doku.php?id=html2pdf:en:v4:accueil
+                        'orientation' => 'P', // landscape or portrait orientation
+                        'format' => 'A4', // format A4, A5, ...
+                        'language' => 'en', // language: fr, en, it ...
+                        'unicode' => true, // TRUE means clustering the input text IS unicode (default = true)
+                        'encoding' => 'UTF-8', // charset encoding; Default is UTF-8
+                        'marges' => array(5, 5, 5, 8), // margins by default, in order (left, top, right, bottom)
+                    )
+                )
+            ),
+        ),
+        /*
+         * end report pdf
+         */
 //        'booster' => array(
 //            'class' => 'ext.booster.components.Booster',
 //            'responsiveCss' => true,
